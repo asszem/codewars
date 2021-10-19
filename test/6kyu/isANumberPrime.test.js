8fbf50e249b4699984ecdf4eb680b2fc4a1ef3f3
Andras Olah <andras.antal.olah@hu.ibm.com>
2021-10-19 15:16:14 +0200

    isANumberPrime - red - tests fail

--- test/6kyu/isANumberPrime.test.js
+++ test/6kyu/isANumberPrime.test.js
@@ -4,4 +4,10 @@ describe('Testing whethe a number is prime...', () => {
   test('when not a prime is given, it should return false', () => {
     expect(isANumberPrime(4)).toBe(false);
   });
+  test('when a prime is given, it should return true', () => {
+    expect(isANumberPrime(13)).toBe(true);
+  });
+  test('when a huge number is given, it should not take too long', () => {
+    expect(isANumberPrime(1111111111111111)).toBe(false);
+  });
 });
